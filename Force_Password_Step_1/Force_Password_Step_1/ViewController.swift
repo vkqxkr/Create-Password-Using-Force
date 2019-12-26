c0a541f86f36588835646a11a06c284571bc12b9
OwnMacBookPro <vkqxkr@gmail.com>
2019-12-26 19:46:00 +0900

    수정

---
+++ Force_Password_Step_1/Force_Password_Step_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Force_Password_Step_1
+//
+//  Created by JinHyuck Park on 2019/12/26.
+//  Copyright © 2019 JinHyuck Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Force_Password_Step_1/Force_Password_Step_1Tests/Force_Password_Step_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Force_Password_Step_1Tests.swift
+//  Force_Password_Step_1Tests
+//
+//  Created by JinHyuck Park on 2019/12/26.
+//  Copyright © 2019 JinHyuck Park. All rights reserved.
+//
+
+import XCTest
+@testable import Force_Password_Step_1
+
+class Force_Password_Step_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
