9c3d311e4bbf8abba2f950103c303c8bf4440ac7
OwnMacBookPro <vkqxkr@gmail.com>
2019-12-27 11:49:31 +0900

    수정

--- Force_Password_Step_1/Force_Password_Step_1/ViewController2.swift
+++ Force_Password_Step_1/Force_Password_Step_1/ViewController2.swift
@@ -13,8 +13,18 @@ class ViewController2: UIViewController {
     @IBOutlet weak var PreviousInputCount_Label: UILabel!
     @IBOutlet weak var PreviousStayTime_Label: UILabel!
     @IBOutlet weak var CurrentInputCount_Label: UILabel!
-    @IBOutlet weak var One_Button: Button!
-    @IBOutlet weak var Two_Button: Button!
+    @IBOutlet weak var One_Button: UIButton!
+    @IBOutlet weak var Two_Button: UIButton!
+    @IBOutlet weak var Three_Button: UIButton!
+    @IBOutlet weak var Four_Button: UIButton!
+    @IBOutlet weak var FIve_Button: UIButton!
+    @IBOutlet weak var Six_Button: UIButton!
+    @IBOutlet weak var Seven_Button: UIButton!
+    @IBOutlet weak var Eight_Button: UIButton!
+    @IBOutlet weak var Nine_Button: UIButton!
+    @IBOutlet weak var Star_Button: UIButton!
+    @IBOutlet weak var Zero_Button: UIButton!
+    @IBOutlet weak var Sharp_Button: UIButton!
     
     var SubjectInfomation_String: String = ""
     var SubjectName_String: String = ""
