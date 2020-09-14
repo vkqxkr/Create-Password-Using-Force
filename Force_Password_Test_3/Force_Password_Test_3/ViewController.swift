149c130d307e99ff07b34259727eb2bac4ac2be7
vkqxkr <36595743+vkqxkr@users.noreply.github.com>
2020-09-14 14:54:45 +0900

    버전 3 추가

---
+++ Force_Password_Test_3/Force_Password_Test_3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Force_Password_Test_3
+//
+//  Created by JinHyuck Park on 2020/09/14.
+//  Copyright © 2020 JinHyuck Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
